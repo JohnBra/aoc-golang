66bd9c16ff20ccfbb087ba907c4d1431a23c069a
Jonathan Braat <jonathan.braat@live.com>
2024-12-03 10:14:54 +1300

    [ADD] zip util

--- day1/main.go
+++ day1/main.go
@@ -4,31 +4,8 @@ import (
 	"fmt"
 	"github.com/JohnBra/aoc-2024/internal/utils"
 	"math"
-	"regexp"
 	"slices"
-	"strconv"
 )
-
-// panics on error
-func getLeftAndRightList(contents [][]rune) ([]int, []int) {
-	re := regexp.MustCompile(`\d+`)
-	var left []int
-	var right []int
-
-	for r := 0; r < len(contents); r++ {
-		line := string(contents[r])
-		nums := re.FindAllString(line, -1)
-
-		leftVal, err := strconv.Atoi(nums[0])
-		utils.Check(err)
-		left = append(left, leftVal)
-
-		rightVal, err := strconv.Atoi(nums[1])
-		utils.Check(err)
-		right = append(right, rightVal)
-	}
-	return left, right
-}
 
 func partOne(left []int, right []int) int {
 	slices.Sort(left)
@@ -60,22 +37,27 @@ func main() {
 	// read lines from input
 	// create two lists left / right respectively
 
-	contents, err := utils.GetFileContentsAsRunes("./input.txt")
+	contents, err := utils.GetFileContentsAsInts("./input.txt")
 	utils.Check(err)
-	left, right := getLeftAndRightList(contents)
+	lists, err := utils.Zip(contents)
+	utils.Check(err)
+
+	if len(lists) != 2 {
+		panic(fmt.Errorf("Input should have two columns exactly"))
+	}
 
 	// day1 part 1
 	// sort both lists asc
 	// init distance = 0
 	// iterate over list abs subtract both values and add to distance
-	distance := partOne(left, right)
+	distance := partOne(lists[0], lists[1])
 	fmt.Printf("Distance is: %d\n", distance)
 
 	// day1 part 2
 	// create map for right list {num -> count}
 	// init similarity = 0
 	// iterate over left list and add leftVal * map[leftVal] to similarity
-	similarity := partTwo(left, right)
+	similarity := partTwo(lists[0], lists[1])
 	fmt.Printf("Similarity score is: %d\n", similarity)
 
 }
