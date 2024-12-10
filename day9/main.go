cdc8ef91aa733c950de7d951e1c444932a6d4fcb
Jonathan Braat <jonathan.braat@live.com>
2024-12-10 14:23:59 +1300

    [ADD] day9 part 2

--- day9/main.go
+++ day9/main.go
@@ -26,7 +26,9 @@ func readFile(filepath string) []rune {
 }
 
 // day9 part 1
-// naive solution is to create empty slice of "file blocks"
+//
+// approach 1:
+// create empty slice of "file blocks"
 // iterate through all runes and append elements to the new slice according
 // to the alternating file block / empty space pattern in puzzle description
 // then use two pointers on that new slice one at start (left) one at end (right)
@@ -36,7 +38,7 @@ func readFile(filepath string) []rune {
 //
 // # O(sum(n)) time and space complexity which could be quite big
 //
-// better:
+// approach 2:
 // init int slice (blocks)
 // two pointers one left, one right of input rune slice
 //
@@ -61,7 +63,6 @@ func partOne(runes []rune) int {
 
 	for l < r {
 		if emptyBlocks == 0 { // fill from left side
-
 			if l%2 == 0 { // file block
 				blocks = append(blocks, slices.Repeat([]int{l / 2}, int(runes[l]-'0'))...)
 			} else {
@@ -69,7 +70,6 @@ func partOne(runes []rune) int {
 			}
 			l += 1
 		} else { // fill empty blocks with file blocks from right side
-			// add file blocks to queue but only if not empty
 			for emptyBlocks > 0 {
 				blocksize := int(runes[r] - '0')
 				if r%2 == 0 {
@@ -99,9 +99,80 @@ func partOne(runes []rune) int {
 	return res
 }
 
-func main() {
-	runes := readFile("9.in")
+// day8 part 2
+// create slice blocks of all file and empty blocks
+// iterate through input from right side until all file blocks tested
+// for every fileblock: search for window with empty blocks of fileblock size
+// if found -> move there
+// calculate checksum like in part one and return result
+func partTwo(runes []rune) int {
+	res := 0
+	blocks := []int{}
 
-	partOneRes := partOne(runes)
-	fmt.Println("Checksum", partOneRes)
+	// first create blocks including empty spaces
+	for i := 0; i < len(runes); i++ {
+		if i%2 == 0 {
+			blocks = append(blocks, slices.Repeat([]int{i / 2}, int(runes[i]-'0'))...)
+		} else {
+			blocks = append(blocks, slices.Repeat([]int{0}, int(runes[i]-'0'))...)
+		}
+	}
+
+	// index for fileblock to move
+	r := len(runes) - 1
+	// lower and upper bound for search window in blocks
+	lower, upper := int(runes[0]-'0'), len(blocks)
+
+	for r > 0 {
+		blocksize := int(runes[r] - '0')
+		upper -= blocksize
+		if r%2 == 0 {
+			emptyBlocks := 0 // empty blocks in window
+			left, right := lower, lower
+
+			// search for window where empty blocks == blocksize
+			for i := lower; i < upper; i++ {
+				if blocks[right] == 0 {
+					emptyBlocks += 1
+				}
+
+				// found window, move file there
+				if blocksize == emptyBlocks {
+					for j := 0; j < blocksize; j++ {
+						blocks[left+j] = blocks[upper+j]
+						blocks[upper+j] = 0
+					}
+					break
+				}
+
+				if right-left+1 >= blocksize {
+					if blocks[left] == 0 {
+						emptyBlocks -= 1
+					}
+					left += 1
+				}
+				right += 1
+			}
+		}
+
+		r -= 1
+	}
+
+	for i, v := range blocks {
+		res += i * v
+	}
+
+	return res
+}
+
+func main() {
+	p1input := readFile("9.in")
+	p2input := make([]rune, len(p1input))
+	_ = copy(p2input, p1input)
+
+	partOneRes := partOne(p1input)
+	fmt.Println("Checksum part one:", partOneRes)
+
+	partTwoRes := partTwo(p2input)
+	fmt.Println("Checksum part two:", partTwoRes)
 }
