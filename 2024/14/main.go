f7b6e33542f39532a69b17b7b97d5726c9cfe966
Jonathan Braat <jonathan.braat@live.com>
2024-12-14 21:26:30 +1300

    [ADD] day14 part 2

--- 2024/14/main.go
+++ 2024/14/main.go
@@ -2,20 +2,59 @@ package main
 
 import (
 	"fmt"
+	"image"
+	"image/color"
+	"image/png"
+	"os"
+
+	"golang.org/x/image/font"
+	"golang.org/x/image/font/basicfont"
+	"golang.org/x/image/math/fixed"
 
 	"github.com/JohnBra/aoc-2024/internal/utils"
 )
 
-func partOne(robots [][]int) int {
-	// map has 103 rows and 101 columns
-	rows, cols := 103, 101
-	i := 100
-	for i > 0 {
-		for _, robot := range robots {
-			robot[1] = (robot[1] + robot[3] + rows) % rows // row
-			robot[0] = (robot[0] + robot[2] + cols) % cols // col
+func addLabel(img *image.RGBA, x, y int, label string) {
+	col := color.RGBA{22, 147, 227, 255}
+	point := fixed.Point26_6{X: fixed.I(x), Y: fixed.I(y)}
+
+	d := &font.Drawer{
+		Dst:  img,
+		Src:  image.NewUniform(col),
+		Face: basicfont.Face7x13,
+		Dot:  point,
+	}
+	d.DrawString(label)
+}
+
+func setBackground(img *image.RGBA, w, h int) {
+	for y := range h {
+		for x := range w {
+			img.Set(x, y, color.RGBA{24, 24, 24, 255})
 		}
-		i--
+	}
+}
+
+func simulateRobots(robots [][]int) int {
+	// map has 103 rows and 101 columns / image is 101 px wide and 103 px high
+	const variations = 100
+	const rows, cols = 103, 101
+	const w, h = cols * variations, rows * variations
+	img := image.NewRGBA(image.Rect(0, 0, w, h))
+	setBackground(img, w, h)
+
+	i := 1
+	for imgRow := range variations {
+		for imgCol := range variations {
+			for _, robot := range robots {
+				robot[1] = (robot[1] + robot[3] + rows) % rows // row
+				robot[0] = (robot[0] + robot[2] + cols) % cols // col
+				img.Set(robot[0]+imgCol*cols, robot[1]+imgRow*rows, color.RGBA{65, 255, 0, 255})
+			}
+
+			addLabel(img, imgCol*cols, 9+imgRow*rows, fmt.Sprintf("%d", i))
+			i++
+		}
 	}
 
 	q1, q2, q3, q4 := 0, 0, 0, 0
@@ -33,6 +72,13 @@ func partOne(robots [][]int) int {
 		}
 	}
 
+	file, err := os.Create("tree-search.png")
+	utils.Check(err)
+	defer file.Close()
+
+	err = png.Encode(file, img)
+	utils.Check(err)
+
 	return q1 * q2 * q3 * q4
 }
 
@@ -41,6 +87,7 @@ func main() {
 	robots, err := utils.GetFileContentsAsIntMatrix(utils.GetPuzzleInputSrc())
 	utils.Check(err)
 
-	partOneRes := partOne(robots)
+	partOneRes := simulateRobots(robots)
 	fmt.Println("Part one res", partOneRes)
+	fmt.Println("Check the generated image for part 2")
 }
--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/JohnBra/aoc-2024
 go 1.23.3
 
 require golang.org/x/exp v0.0.0-20241204233417-43b7b7cde48d
+
+require golang.org/x/image v0.23.0 // indirect
