b4a093d84a37d2fc5ee6b75bbb74ce21a9a21fd9
Jonathan Braat <jonathan.braat@live.com>
2024-12-16 12:01:34 +1300

    [ADD] day15 part 2 test works

--- 2024/15/main.go
+++ 2024/15/main.go
@@ -15,7 +15,7 @@ var dir = map[rune][2]int{
 	'<': {0, -1},
 }
 
-func moveRobot(matrix [][]rune, r, c int, move rune) bool {
+func moveRobotOne(matrix [][]rune, r, c int, move rune) bool {
 	// should never be out of bounds
 	if utils.IsOutOfBounds(matrix, r, c) || matrix[r][c] == '#' {
 		return false
@@ -26,11 +26,74 @@ func moveRobot(matrix [][]rune, r, c int, move rune) bool {
 	}
 
 	nextR, nextC := r+dir[move][0], c+dir[move][1]
-	push := moveRobot(matrix, nextR, nextC, move)
+	push := moveRobotOne(matrix, nextR, nextC, move)
 	if push {
 		matrix[nextR][nextC] = matrix[r][c]
 	}
 	return push
+}
+
+func moveRobotTwo(matrix [][]rune, r, c int, move rune) bool {
+	// should never be out of bounds
+	if utils.IsOutOfBounds(matrix, r, c) || matrix[r][c] == '#' {
+		return false
+	}
+
+	if matrix[r][c] == '.' {
+		return true
+	}
+
+	push := false
+
+	if move == '<' || move == '>' {
+		nextR, nextC := r+dir[move][0], c+dir[move][1]
+		push = moveRobotTwo(matrix, nextR, nextC, move)
+		if push {
+			matrix[nextR][nextC] = matrix[r][c]
+			matrix[r][c] = '.'
+		}
+	} else {
+		nei := 0
+
+		if matrix[r][c] == '[' {
+			nei = 1
+		} else if matrix[r][c] == ']' {
+			nei = -1
+		}
+
+		nextR, nextC := r+dir[move][0], c+dir[move][1]
+		push = moveRobotTwo(matrix, nextR, nextC, move) && moveRobotTwo(matrix, nextR, nextC+nei, move)
+		if push {
+			matrix[nextR][nextC] = matrix[r][c]
+			matrix[nextR][nextC+nei] = matrix[r][c+nei]
+			matrix[r][c] = '.'
+			matrix[r][c+nei] = '.'
+		}
+	}
+	return push
+}
+
+func widenMatrix(matrix [][]rune) [][]rune {
+	wMatrix := [][]rune{}
+	for r := range len(matrix) {
+		row := []rune{}
+		for c := range len(matrix[0]) {
+			var a, b rune
+			switch matrix[r][c] {
+			case '#':
+				a, b = '#', '#'
+			case 'O':
+				a, b = '[', ']'
+			case '.':
+				a, b = '.', '.'
+			case '@':
+				a, b = '@', '.'
+			}
+			row = append(row, a, b)
+		}
+		wMatrix = append(wMatrix, row)
+	}
+	return wMatrix
 }
 
 func getRobotPosition(matrix [][]rune) [2]int {
@@ -50,17 +113,49 @@ func partOne(matrix [][]rune, moves []rune) int {
 
 	for _, m := range moves {
 		nextR, nextC := robot[0]+dir[m][0], robot[1]+dir[m][1]
-		if moveRobot(matrix, nextR, nextC, m) {
+		if moveRobotOne(matrix, nextR, nextC, m) {
 			matrix[robot[0]][robot[1]] = '.'
 			robot[0], robot[1] = nextR, nextC
 			matrix[robot[0]][robot[1]] = '@'
 		}
-
 	}
 
 	for r := range len(matrix) {
 		for c := range len(matrix[0]) {
 			if matrix[r][c] == 'O' {
+				res += r*100 + c
+			}
+		}
+	}
+
+	return res
+}
+
+func partTwo(matrix [][]rune, moves []rune) int {
+	res := 0
+	robot := getRobotPosition(matrix)
+	fmt.Println("part 2 start")
+	for _, row := range matrix {
+		fmt.Println(string(row))
+	}
+
+	for _, m := range moves {
+		nextR, nextC := robot[0]+dir[m][0], robot[1]+dir[m][1]
+		if moveRobotTwo(matrix, nextR, nextC, m) {
+			matrix[robot[0]][robot[1]] = '.'
+			robot[0] += dir[m][0]
+			robot[1] += dir[m][1]
+			matrix[robot[0]][robot[1]] = '@'
+		}
+	}
+
+	for _, row := range matrix {
+		fmt.Println(string(row))
+	}
+
+	for r := range len(matrix) {
+		for c := range len(matrix[0]) {
+			if matrix[r][c] == '[' {
 				res += r*100 + c
 			}
 		}
@@ -107,7 +202,11 @@ func parseInput(filepath string) ([][]rune, []rune, error) {
 func main() {
 	matrix, moves, err := parseInput(utils.GetPuzzleInputSrc())
 	utils.Check(err)
+	wMatrix := widenMatrix(matrix)
 
 	partOneRes := partOne(matrix, moves)
 	fmt.Println("Part one res:", partOneRes)
+
+	partTwoRes := partTwo(wMatrix, moves)
+	fmt.Println("Part two res:", partTwoRes)
 }
