71f4c07b7a5fb5ada79d66ccdcb6eee702b9678a
Jonathan Braat <jonathan.braat@live.com>
2024-12-19 14:29:15 +1300

    [REF] 16

--- 2024/16/main.go
+++ 2024/16/main.go
@@ -17,9 +17,13 @@ func getCost(item []int, orientation int) int {
 	return item[0] + 1 + diff*1000
 }
 
-func walkMaze(matrix [][]rune) int {
-	res := math.MaxInt
-	visit := ds.NewSet[[3]int]()
+func walkMaze(matrix [][]rune) (int, int) {
+	resCost := math.MaxInt
+	// map with (r, c, o) -> min cost to get there
+	lowestCost := ds.Map[[3]int, int]{}
+	// record of optimal predecessors to specific location with orientation
+	optimalPaths := ds.Map[[3]int, ds.Set[[3]int]]{}
+	endStates := ds.Set[[3]int]{}
 
 	// minheap with items [cost, r, c, orientation]
 	h := &ds.IntHeap{}
@@ -28,25 +32,60 @@ func walkMaze(matrix [][]rune) int {
 
 	for h.Len() > 0 {
 		item := heap.Pop(h).([]int)
-		visit.Add([3]int{item[1], item[2], item[3]})
+		cur := [3]int{item[1], item[2], item[3]}
+
+		if item[0] > lowestCost.Get(cur, math.MaxInt) {
+			continue
+		}
 
 		if matrix[item[1]][item[2]] == 'E' {
-			if item[0] > res {
+			if item[0] > resCost {
 				break
 			}
-			res = utils.Min(res, item[0])
+			endStates.Add(cur)
+			resCost = item[0]
 		}
 
 		for o, dir := range utils.Axes {
 			nr, nc := item[1]+dir[0], item[2]+dir[1]
-			opposite := (item[3] + 2) % 4 // opposite direction to current orientation
-			if matrix[nr][nc] != '#' && o != opposite && !visit.Contains([3]int{nr, nc, o}) {
-				heap.Push(h, []int{getCost(item, o), nr, nc, o})
+			next := [3]int{nr, nc, o}
+			opposite := (item[3] + 2) % 4 // opposite direction of current orientation
+			ncost := getCost(item, o)     // cost to get to nr, nc with new orientation
+			lcost := lowestCost.Get(next, math.MaxInt)
+			if matrix[nr][nc] != '#' && o != opposite && ncost <= lcost {
+				if ncost < lcost {
+					optimalPaths[next] = ds.NewSet[[3]int]()
+					lowestCost[next] = ncost
+				}
+				optimalPaths[next].Add(cur)
+				heap.Push(h, []int{ncost, nr, nc, o})
 			}
 		}
 	}
 
-	return res
+	states, visit := ds.NewDeque([][3]int{}), ds.NewSet[[3]int]()
+
+	for _, s := range endStates.Members() {
+		states.PushBack(s)
+		visit.Add(s)
+	}
+
+	for states.Len() > 0 {
+		key, _ := states.PopFront()
+		for prev := range optimalPaths.Get(key, ds.NewSet[[3]int]()) {
+			if !visit.Contains(prev) {
+				visit.Add(prev)
+				states.PushBack(prev)
+			}
+		}
+	}
+
+	seats := ds.NewSet[[2]int]()
+	for _, v := range visit.Members() {
+		seats.Add([2]int{v[0], v[1]})
+	}
+
+	return resCost, len(seats)
 }
 
 func parseLine(line string) ([]rune, error) {
@@ -57,6 +96,7 @@ func main() {
 	matrix, err := utils.GetSliceOfSlicesFromFile(utils.GetPuzzleInputSrc(), parseLine)
 	utils.Check(err)
 
-	partOneRes := walkMaze(matrix)
+	partOneRes, partTwoRes := walkMaze(matrix)
 	fmt.Println("Part one res", partOneRes)
+	fmt.Println("Part two res", partTwoRes)
 }
---
+++ internal/datastructures/map.go
@@ -0,0 +1,20 @@
+package datastructures
+
+import "fmt"
+
+type Map[T comparable, U any] map[T]U
+
+// utility function to retrieve a value with an optional fallback
+//
+// can provide 0..1 fallback; panics on multiple fallbacks
+func (m Map[T, U]) Get(key T, fallback ...U) U {
+	val, ok := m[key]
+	if !ok {
+		if len(fallback) == 1 {
+			return fallback[0]
+		} else if len(fallback) > 1 {
+			panic(fmt.Errorf("can't provide more than one fallback value to get"))
+		}
+	}
+	return val
+}
